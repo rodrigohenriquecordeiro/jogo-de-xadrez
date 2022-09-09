8e11ce7fe285c29e7ffa9a16569386ccc8525b1e
Rodrigo H. Cordeiro <rodrigohenriquecordeiro@gmail.com>
2022-09-09 19:55:28 -0300

    Implementado as restricoes de movimento

--- xadrez-console/xadrez/Torre.cs
+++ xadrez-console/xadrez/Torre.cs
@@ -59,7 +59,7 @@ namespace xadrez
             {
                 mat[pos.linha, pos.coluna] = true;
                 if (tab.peca(pos) != null && tab.peca(pos).cor != cor) break;
-                pos.linha = pos.linha - 1;
+                pos.coluna = pos.coluna - 1;
             }
 
             return mat;
